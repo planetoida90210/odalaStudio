66a8b7251de290acf469986c5b28ef33862fb5ba
Michał Bryk <galeriakoloru90210@gmail.com>
2023-10-23 15:33:55 +0200

    initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+
+const navigation = {
+	sklep: [
+		{ name: "Ubrania", href: "/" },
+		{ name: "Płyty", href: "/" },
+		{ name: "Beaty", href: "/" },
+		{ name: "Nowości", href: "/" },
+	],
+	pomoc: [
+		{ name: "Polityka prywatności", href: "/" },
+		{ name: "Regulamin", href: "/" },
+		{ name: "Zwroty i reklamacje", href: "/" },
+		{ name: "Kontakt", href: "/" },
+	],
+	informacje: [
+		{ name: "O nas", href: "/" },
+		{ name: "Blog", href: "/" },
+		{ name: "Praca", href: "/" },
+		{ name: "Współpraca", href: "/" },
+	],
+	ustalenia: [
+		{ name: "Prawa autorskie", href: "/" },
+		{ name: "Warunki korzystania", href: "/" },
+		{ name: "Ochrona danych", href: "/" },
+	],
+};
+
+export const Footer = () => {
+	return (
+		<footer className="bg-zinc-950" aria-labelledby="footer-heading">
+			<h3 id="footer-heading" className="sr-only">
+				Footer
+			</h3>
+			<div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8 lg:py-32">
+				<div className="xl:grid xl:grid-cols-3 xl:gap-8">
+					<Link href={"/"} className="block w-fit">
+						<span className="block -rotate-6 bg-zinc-50 p-2 text-3xl font-semibold text-zinc-950">
+							OdalaStudios
+						</span>
+					</Link>
+					<div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+						{Object.entries(navigation).map(([category, links]) => (
+							<div key={category}>
+								<h3 className="text-sm font-semibold capitalize leading-6 text-zinc-100">
+									{category}
+								</h3>
+								<ul role="list" className="mt-6 space-y-4">
+									{links.map((item) => (
+										<li key={item.name}>
+											<a
+												href={item.href}
+												className="text-sm leading-6 text-zinc-300 hover:text-[#ce9f2c]"
+											>
+												<p className="capitalize">{item.name}</p>
+											</a>
+										</li>
+									))}
+								</ul>
+							</div>
+						))}
+					</div>
+				</div>
+				<p className="mt-20 text-center text-sm text-zinc-200">
+					&copy; {new Date().getFullYear()} OdalaStudios. All rights reserved.
+				</p>
+			</div>
+		</footer>
+	);
+};
---
+++ src/components/MainBanner.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export const MainBanner = () => {
+	return (
+		<div className="relative">
+			<div aria-hidden="true" className="absolute hidden h-full w-1/2 bg-zinc-200 lg:block" />
+			<div className="relative bg-zinc-100 lg:bg-transparent">
+				<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:grid lg:grid-cols-2 lg:px-8">
+					<div className="mx-auto max-w-2xl py-24 lg:max-w-none lg:py-64">
+						<div className="lg:pr-16">
+							<h1 className="text-4xl font-bold tracking-tight text-zinc-900 sm:text-5xl xl:text-6xl">
+								Miejsce w którym liczy się muzyka.
+							</h1>
+							<p className="mt-4 text-xl text-zinc-600">
+								Poznaj świat muzyki z Odalastudios, odkryj unikalne ciuchy, które mamy w ofercie.
+							</p>
+							<div className="mt-6">
+								<Link href="/">
+									<Button size={"lg"}>Przejdź do sklepu</Button>
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="h-48 w-full sm:h-64 lg:absolute lg:right-0 lg:top-0 lg:h-full lg:w-1/2">
+				<Image
+					width={1080}
+					height={1080}
+					src="/images/studio1.jpeg"
+					alt=""
+					className="h-full w-full object-cover object-center"
+					priority
+				/>
+			</div>
+		</div>
+	);
+};
