b4ad170af722a99b5baab9cf43891c8986fd07c1
Michał Bryk <galeriakoloru90210@gmail.com>
2023-10-23 18:03:46 +0200

    navbar ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Footer } from "@/components/Footer";
+import { Navbar } from "@/components/Navbar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="pl">
 			<body className={inter.className}>
+				<Navbar />
 				{children}
 				<Footer />
 			</body>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { UrlObject } from "url";
+import { LucideShoppingBag } from "lucide-react";
+import Link from "next/link";
+import { ActiveLink } from "@/components/ActiveLink";
+
+import { Button } from "@/components/ui/button";
+
+const numOfItemsInBag = 0;
+
+export const Navbar = () => {
+	return (
+		<nav className="mx-auto flex max-w-7xl items-center justify-between gap-4 p-6 py-10">
+			<Link href={"/"}>
+				<span className="block -rotate-6 bg-zinc-950 p-2 text-3xl font-semibold text-zinc-50">
+					Odala Studio
+				</span>
+			</Link>
+			<ul className="flex items-center gap-4" role="navigation">
+				<ActiveLink
+					href={"/"}
+					className="border-b-2 border-b-transparent text-lg"
+					activeClassName="border-b-2 border-zinc-900 text-lg font-semibold"
+				>
+					Nowości
+				</ActiveLink>
+				<ActiveLink
+					href={"/products"}
+					className="border-b-2 border-b-transparent text-lg"
+					activeClassName="border-b-2 border-zinc-900 text-lg font-semibold"
+				>
+					Wszystko
+				</ActiveLink>
+				<ActiveLink
+					href={"/studio"}
+					className="border-b-2 border-b-transparent text-lg"
+					activeClassName="border-b-2 border-zinc-900 text-lg font-semibold"
+				>
+					Studio
+				</ActiveLink>
+			</ul>
+			<Link href={"/cart"} role="button">
+				<Button variant={"ghost"} className="flex gap-1 px-2">
+					<LucideShoppingBag className={"h-6 w-6 "} />
+					<span>{numOfItemsInBag}</span>
+				</Button>
+			</Link>
+		</nav>
+	);
+};
