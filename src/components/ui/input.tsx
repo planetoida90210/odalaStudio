3a42f7d2e11d42cb8731f2047192d79c3263ca29
Michał Bryk <galeriakoloru90210@gmail.com>
2023-11-06 01:16:57 +0100

    change shadcn to previous

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,8 +11,6 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 				type={type}
 				className={cn(
 					"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-					"sm:mb-0 sm:mr-0 sm:scale-100 sm:font-medium",
-					"mb-[-25%] mr-[-25%] scale-75 font-[16px]",
 					className,
 				)}
 				ref={ref}
