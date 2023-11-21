5e60e02c5c23779ae7bda3fc9a0e02ca7c4f56ce
Michał Bryk <galeriakoloru90210@gmail.com>
2023-11-21 23:16:42 +0100

    change getstaticprops to getstaticpaths

--- src/app/products/[page]/page.tsx
+++ src/app/products/[page]/page.tsx
@@ -1,13 +1,18 @@
 import { getProductsList } from "@/api/products";
 import { ProductList } from "@/components/ProductList";
 
-export async function generateStaticParams() {
+export async function getStaticPaths() {
 	const products = await getProductsList(0, 10);
 	const numOfPages = Math.ceil(products.length / 10);
-	const pages = Array.from({ length: numOfPages }, (_, i) => i + 1);
-	return pages.map((page) => ({ params: { page: page.toString() } }));
-}
+	const paths = Array.from({ length: numOfPages }, (_, i) => ({
+		params: { page: (i + 1).toString() },
+	}));
 
+	return {
+		paths,
+		fallback: "blocking",
+	};
+}
 export default async function ProductsPage({ params }: { params: { page: string } }) {
 	const skip = Number(params.page) === 1 ? 0 : (Number(params.page) - 1) * 10;
 	const products = await getProductsList(10, skip);
