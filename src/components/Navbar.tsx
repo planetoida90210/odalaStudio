b4c1f2a65408e897b1587d9de152d886f12ba1b7
Michał Bryk <galeriakoloru90210@gmail.com>
2023-11-23 22:10:17 +0100

    change mobile view to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,14 +17,16 @@ export const Navbar = async () => {
 	return (
 		<nav
 			id="navbar"
-			className="navbar mx-auto flex h-[120px] max-w-7xl items-center justify-center gap-4 p-6 py-10 md:h-[180px] md:items-center"
+			className="navbar mx-auto flex h-[120px] w-full items-center justify-between gap-6 p-6 py-10 md:h-[180px] lg:max-w-7xl lg:items-center lg:justify-center"
 		>
 			<Link href={"/"}>
 				<span className="mt-2 block -rotate-6 bg-zinc-950 p-2 font-semibold text-zinc-50 md:text-3xl">
 					Odala Studio
 				</span>
 			</Link>
-			<SearchInput />
+			<div className="hidden lg:block">
+				<SearchInput />
+			</div>
 			<ul className="hidden items-center gap-4 space-x-2 md:flex" role="navigation">
 				<ActiveLink
 					href={"/products"}
@@ -53,25 +55,27 @@ export const Navbar = async () => {
 					Studio
 				</ActiveLink>
 			</ul>
-			<div className="md:hidden">
-				<SidebarToggle />
-			</div>
-			<Link href={"/cart"} role="button">
-				<Button variant={"ghost"} className="flex gap-1 px-2">
-					<LucideShoppingBag className={"h-6 w-6 "} />
-					<span>{numOfItemsInBag}</span>
-				</Button>
-			</Link>
-			<SignedIn>
-				<UserButton afterSignOutUrl="/" />
-			</SignedIn>
-			<SignedOut>
-				<Link href="/sign-in">
+			<div className="flex flex-grow items-center justify-end">
+				<Link href={"/cart"} role="button">
 					<Button variant={"ghost"} className="flex gap-1 px-2">
-						<LucideUser className={"h-6 w-6 "} />
+						<LucideShoppingBag className={"h-6 w-6 "} />
+						<span>{numOfItemsInBag}</span>
 					</Button>
 				</Link>
-			</SignedOut>
+				<SignedIn>
+					<UserButton afterSignOutUrl="/" />
+				</SignedIn>
+				<SignedOut>
+					<Link href="/sign-in">
+						<Button variant={"ghost"} className="flex gap-1 px-2">
+							<LucideUser className={"h-6 w-6 "} />
+						</Button>
+					</Link>
+				</SignedOut>
+				<div className="md:hidden">
+					<SidebarToggle />
+				</div>
+			</div>
 		</nav>
 	);
 };
