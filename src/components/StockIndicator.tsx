ddf0c19c5dc4b13f1422e571bfc313aa42a0097d
Michał Bryk <galeriakoloru90210@gmail.com>
2023-11-24 15:58:54 +0100

    fix stock indicator for ui rendering

--- src/components/StockIndicator.tsx
+++ src/components/StockIndicator.tsx
@@ -1,13 +1,31 @@
 export const StockIndicator = ({ stock }: { stock: number | null | undefined }) => {
-	let stockMessage = "Informacja o stanie magazynowym niedostępna";
+	const renderStockNumber = () => {
+		if (stock === null || stock === undefined) {
+			return (
+				<span className="font-base text-sm text-zinc-600">
+					Informacja o stanie magazynowym niedostępna
+				</span>
+			);
+		}
 
-	if (typeof stock === "number") {
-		stockMessage = stock > 0 ? `Na stanie: ${stock}` : "Brak w magazynie";
-	}
+		if (stock <= 0) {
+			return <span className="font-base text-sm text-zinc-600">Brak w magazynie</span>;
+		}
 
-	return (
-		<div className="flex items-center justify-center">
-			<span className="text-sm font-semibold text-zinc-800">{stockMessage}</span>
-		</div>
-	);
+		const isLowStock = stock < 3;
+		const stockClass = isLowStock ? "text-red-600" : "text-zinc-600";
+		const lowStockMessage = stock === 1 ? " - ostatnia sztuka!" : " - ostatnie sztuki!";
+
+		return (
+			<div className="flex gap-2">
+				<span className="font-base text-sm text-zinc-600">Na stanie: </span>
+				<span className={`font-base text-sm ${stockClass}`}>
+					{stock}
+					{isLowStock ? lowStockMessage : ""}
+				</span>
+			</div>
+		);
+	};
+
+	return <div className="flex items-center justify-center">{renderStockNumber()}</div>;
 };
