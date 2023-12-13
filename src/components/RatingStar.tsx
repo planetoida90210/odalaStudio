8d6429dac4afe8e066fa03f529b8046119daf77b
Michał Bryk <galeriakoloru90210@gmail.com>
2023-12-13 21:22:13 +0100

    add ui stars to review

--- src/components/RatingStar.tsx
+++ src/components/RatingStar.tsx
@@ -1,14 +1,15 @@
 import clsx from "clsx";
-import { StarsIcon } from "lucide-react";
+import { StarIcon } from "lucide-react";
 
 export const RatingStar = ({ isActive, onClick }: { isActive: boolean; onClick: () => void }) => {
 	return (
 		<div onClick={onClick}>
-			<StarsIcon
+			<StarIcon
+				fill={isActive ? "#f9bc00" : "#D1D5DB"}
 				className={clsx(
-					"h-4 w-4",
-					`${!isActive && "text-neutral-200"}`,
-					`${isActive && "text-amber-600"}`,
+					"h-4 w-4 cursor-pointer",
+					`${!isActive && "text-zinc-300"}`,
+					`${isActive && "text-[#f9bc00]"}`,
 				)}
 				aria-hidden="true"
 			/>
--- src/components/RatingStarsForm.tsx
+++ src/components/RatingStarsForm.tsx
@@ -10,7 +10,7 @@ export const RatingStarsForm = ({
 	return (
 		<fieldset name="rating" className="flex items-center space-x-2">
 			<input type="hidden" name="rating" value={value} />
-			Rating:
+			Ocena:
 			{Array.from({ length: 5 }, (_, i) => i + 1).map((i) => (
 				<RatingStar key={i} isActive={i <= value} onClick={() => onClick(i)} />
 			))}
